ef2df1fe67ab9ea261aa19d15a7b6cb8f534583a
ili101 <zoby101@gmail.com>
2019-10-06 23:53:59 +0300

    t3

--- Tests/Users.Tests.ps1
+++ Tests/Users.Tests.ps1
@@ -7,6 +7,9 @@ param (
 Write-Host 'p1'
 if ($env:AGENT_NAME) {
     Write-Host 'p11'
+    "NextcloudUser {0}" -f ([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$(NextcloudUser)")))
+    "System_JobDisplayName {0}" -f ([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$(NextcloudServer)")))
+
     $Credential = [Management.Automation.PSCredential]::new("$(NextcloudUser)", (ConvertTo-SecureString "$(NextcloudPassword)" -AsPlainText -Force))
     Write-Host 'p12'
     $Server = "$(NextcloudServer)"
